0701ee52f5621a5dae474175b86c20bfee087498
Wandao123 <20665675+Wandao123@users.noreply.github.com>
2019-03-25 17:12:40 +0900

    編隊を作成
    コルーチンを使って複数生成するようにした。しかし、Luaではスレッド（コルーチンとは異なる）をサポートしていないらしく、醜い書き方になってしまった。

--- scripts/stage.lua
+++ scripts/stage.lua
@@ -1,18 +1,31 @@
 function StartStage()
+	local co = {}
+	for i = 1, 5 do
+		co[i] = coroutine.create(smallBlueEnemy)
+		for j = 1, 15 do           -- 15フレーム待つ。
+			for k = 1, #co do  -- 他の敵を生成している間も更新。
+				coroutine.resume(co[k])
+			end
+			coroutine.yield()
+		end
+	end
+	while true do
+		for i = 1, #co do
+			if coroutine.status(co[i]) ~= 'dead' then
+				coroutine.resume(co[i])
+			end
+		end
+		coroutine.yield()
+	end
+end
+
+function smallBlueEnemy()
 	local speed = 2.0
 	local angle = math.pi / 2
 	local enemy = GenerateEnemy('SmallBlue', ScreenWidth / 4, 0, speed, angle)
 	for i = 1, 120 do
 		coroutine.yield()
 	end
-
-	--[[local numDivision = 20
-	local dt = math.pi / 2 / 10
-	for i = 0, numDivision do
-		SetAngle(enemy, angle - dt * i)
-		coroutine.yield()
-	end]]
-
 	local radius = 30.0
 	local diffAngle = math.acos(1.0 - 0.5 * speed ^ 2 / radius ^ 2)
 	angle = angle - diffAngle / 2
