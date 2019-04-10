689505057e5e32f57823945cb91af7adad35982a
Wandao123 <20665675+Wandao123@users.noreply.github.com>
2019-04-10 16:01:11 +0900

    コメントとアニメーションの条件式を修正

--- shooter2d/scene.cpp
+++ shooter2d/scene.cpp
@@ -13,7 +13,7 @@ GameScene::GameScene()
 	// Luaの初期化。
 	lua.open_libraries(sol::lib::base, sol::lib::package, sol::lib::coroutine, sol::lib::math, sol::lib::io, sol::lib::string);
 	lua.script_file("scripts/stage.lua");  // TODO: エラー処理
-	int width = Game::Width, height = Game::Height;  // 磁化に代入するとgccでコンパイルできない。
+	int width = Game::Width, height = Game::Height;  // 直に代入するとgccでコンパイルできない。
 	lua["ScreenWidth"] = width;
 	lua["ScreenHeight"] = height;
 
