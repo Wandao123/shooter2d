adb489cd2ac2cd19971aa2f9f66ed3da541a476b
Wandao123 <20665675+Wandao123@users.noreply.github.com>
2020-03-21 17:00:53 +0900

    Colliderクラスをリファクタリング

--- shooter2d/collider.cpp
+++ shooter2d/collider.cpp
@@ -1,29 +1,6 @@
 #include "collider.h"
-#include <array>
 
 using namespace Shooter;
-
-/******************************** Collider *********************************/
-
-const Vector2<double>& Collider::GetOffset() const&
-{
-	return offset;
-}
-
-void Collider::SetOffset(const Vector2<double>& offset)
-{
-	this->offset = offset;
-}
-
-double Collider::GetAngle() const
-{
-	return angle;
-}
-
-void Collider::SetAngle(const double angle)
-{
-	this->angle = MathUtils::Modulo(angle, 2 * M_PI);  // 負の値が渡されても、0 <= angle < 2 pi になるように変換する。
-}
 
 /******************************** LineSegmentCollider *********************************/
 
@@ -43,15 +20,15 @@ bool LineSegmentCollider::DoesCollideWith(const Vector2<double>& relativePositio
 bool LineSegmentCollider::CheckFor(const Vector2<double>& relativePosition, const LineSegmentCollider& lineSegment) const
 {
 	const double EPS = 1.e-8;
-	auto direction1 = MathUtils::PolarCoordinateToVector<double>(length, angle);
-	auto direction2 = relativePosition + MathUtils::PolarCoordinateToVector<double>(lineSegment.GetLength(), lineSegment.GetAngle());
-	auto differenceBetweenInitialPoints = direction1 * 0.5e0 + relativePosition - direction2 * 0.5e0;
-	if (direction1.Cross(direction2) < EPS)
+	auto orientation1 = this->endPoints[1] - this->endPoints[0];
+	auto orientation2 = relativePosition + lineSegment.endPoints[1] - lineSegment.endPoints[0];
+	auto differenceBetweenInitialPoints = lineSegment.endPoints[0] - this->endPoints[0];
+	if (orientation1.Cross(orientation2) < EPS)
 		return false;
-	if (differenceBetweenInitialPoints.Cross(direction2) / direction1.Cross(direction2) >= 0.e0
-			&& differenceBetweenInitialPoints.Cross(direction2) / direction1.Cross(direction2) <= 1.e0
-			&& differenceBetweenInitialPoints.Cross(direction1) / direction1.Cross(direction2) >= 0.e0
-			&& differenceBetweenInitialPoints.Cross(direction1) / direction1.Cross(direction2) <= 1.e0)
+	if (differenceBetweenInitialPoints.Cross(orientation2) / orientation1.Cross(orientation2) >= 0.e0
+			&& differenceBetweenInitialPoints.Cross(orientation2) / orientation1.Cross(orientation2) <= 1.e0
+			&& differenceBetweenInitialPoints.Cross(orientation1) / orientation1.Cross(orientation2) >= 0.e0
+			&& differenceBetweenInitialPoints.Cross(orientation1) / orientation1.Cross(orientation2) <= 1.e0)
 		return true;
 	return false;
 }
@@ -62,13 +39,13 @@ bool LineSegmentCollider::CheckFor(const Vector2<double>& relativePosition, cons
 /// <remarks>relativePositionは自分を基準点とする。</remarks>
 bool LineSegmentCollider::CheckFor(const Vector2<double>& relativePosition, const CircleCollider& circle) const
 {
-	auto direction = MathUtils::PolarCoordinateToVector<double>(length, angle);
-	if (std::abs(direction.Normalize().Cross(relativePosition)) > circle.GetRadius())
+	auto orientation = endPoints[1] - endPoints[0];
+	if (std::abs(orientation.Normalize().Cross(relativePosition)) > circle.GetRadius())
 		return false;
-	if ((relativePosition + direction * 0.5e0).SquaredMagnitude() < std::pow(circle.GetRadius(), 2.e0)
-			|| (relativePosition - direction * 0.5e0).SquaredMagnitude() < std::pow(circle.GetRadius(), 2.e0))
+	if ((relativePosition + orientation * 0.5e0).SquaredMagnitude() < std::pow(circle.GetRadius(), 2.e0)
+			|| (relativePosition - orientation * 0.5e0).SquaredMagnitude() < std::pow(circle.GetRadius(), 2.e0))
 		return true;
-	if ((relativePosition + direction * 0.5e0).Dot(direction) * (relativePosition + direction * 0.5e0).Dot(direction) <= 0.e0)
+	if ((relativePosition + orientation * 0.5e0).Dot(orientation) * (relativePosition + orientation * 0.5e0).Dot(orientation) <= 0.e0)
 		return true;
 	return false;
 }
@@ -79,54 +56,26 @@ bool LineSegmentCollider::CheckFor(const Vector2<double>& relativePosition, cons
 /// <remarks>relativePositionは自分を基準点とする。</remarks>
 bool LineSegmentCollider::CheckFor(const Vector2<double>& relativePosition, const RectangleCollider& rectangle) const
 {
-	auto direction = MathUtils::PolarCoordinateToVector<double>(length, angle);
-	std::array<Vector2<double>, 2> endPoints = { relativePosition + direction * 0.5e0, relativePosition - direction * 0.5e0 };
-	std::array<Vector2<double>, 5> rect = {  // 相手の四隅の座標。隣同士の差を取ると四辺のベクトルになるようにする。
-		relativePosition + (Vector2<double>{ -rectangle.GetWidth(), -rectangle.GetHeight() } * 0.5).Rotate(rectangle.GetAngle()),
-		relativePosition + (Vector2<double>{ -rectangle.GetWidth(), rectangle.GetHeight() } * 0.5).Rotate(rectangle.GetAngle()),
-		relativePosition + (Vector2<double>{ rectangle.GetWidth(), -rectangle.GetHeight() } * 0.5).Rotate(rectangle.GetAngle()),
-		relativePosition + (Vector2<double>{ rectangle.GetWidth(), rectangle.GetHeight() } * 0.5).Rotate(rectangle.GetAngle()),
-		relativePosition + (Vector2<double>{ -rectangle.GetWidth(), -rectangle.GetHeight() } * 0.5).Rotate(rectangle.GetAngle())
-	};
+	auto endPoints = GetEndPoints();
+	auto vertices = rectangle.GetVertices();
 
 	// 線分の端点が矩形に含まれる場合。
 	for (std::size_t i = 0; i < endPoints.size(); i++) {
 		auto result = true;
-		for (std::size_t j = 1; j < rect.size(); j++)
-			result = result && ((rect[j] - rect[j - 1]).Cross(endPoints[i] - rect[j - 1]) <= 0.e0);
+		for (std::size_t j = 1; j < vertices.size(); j++)
+			result = result && ((vertices[j] - vertices[j - 1]).Cross(endPoints[i] - vertices[j - 1]) <= 0.e0);
 		if (result)
 			return true;
 	}
 
 	// 線分と矩形の辺とが交差する場合。
-	for (std::size_t i = 1; i < rect.size(); i++) {
-		auto diff = rect[i] - rect[i - 1];
+	for (std::size_t i = 1; i < vertices.size(); i++) {
+		auto diff = vertices[i] - vertices[i - 1];
 		auto segment = LineSegmentCollider({ 0.e0, 0.e0 }, std::atan2(diff.y, diff.x), diff.Magnitude());
-		if (CheckFor(relativePosition + (rect[i] + rect[i - 1]) * 0.5e0, segment))
+		if (CheckFor(relativePosition + (vertices[i] + vertices[i - 1]) * 0.5e0, segment))
 			return true;
 	}
 	return false;
-}
-
-/*/// <summary>方向ベクトルを取得する。</summary>
-/// <returns>方向ベクトルへの参照</returns>
-const Vector2<double>& LineSegmentCollider::GetOrientation() const&
-{
-	return orientation;
-}
-
-
-/// <summary>終点を取得する。</summary>
-/// <returns>終点のベクトル</returns>
-/// <remarks>始点を取得するにはGetOffsetを使う。</remarks>
-Vector2<double> LineSegmentCollider::GetTerminalPoint() const
-{
-	return offset + orientation;
-}*/
-
-double LineSegmentCollider::GetLength() const
-{
-	return length;
 }
 
 /******************************** CircleCollider *********************************/
@@ -175,29 +124,20 @@ bool CircleCollider::CheckFor(const Vector2<double>& relativePosition, const Cir
 /// <remarks>relativePositionは自分を基準点とする。</remarks>
 bool CircleCollider::CheckFor(const Vector2<double>& relativePosition, const RectangleCollider& rectangle) const
 {
-	// 矩形の中心を座標系の原点に取る。
+	// 矩形の中心を座標系の原点に取る。また、矩形が各座標軸と平行になるように回転させる。
 	auto circPos = (-relativePosition).Rotate(-rectangle.GetAngle());
-	if (circPos.x > -rectangle.GetWidth() * 0.5 && circPos.x < rectangle.GetWidth() * 0.5
-			&& circPos.y > -rectangle.GetHeight() * 0.5 - radius && circPos.y < rectangle.GetHeight() * 0.5 + radius)
+	auto size = rectangle.GetSize();
+	if (circPos.x > -size.x * 0.5e0 && circPos.x < size.x * 0.5e0
+			&& circPos.y > -size.y * 0.5e0 - radius && circPos.y < size.y * 0.5e0 + radius)
 		return true;
-	else if (circPos.x > -rectangle.GetWidth() * 0.5 - radius && circPos.x < rectangle.GetWidth() * 0.5 + radius
-			&& circPos.y > -rectangle.GetHeight() * 0.5 && circPos.y < rectangle.GetHeight() * 0.5)
+	else if (circPos.x > -size.x * 0.5e0 - radius && circPos.x < size.x * 0.5e0 + radius
+			&& circPos.y > -size.y * 0.5e0 && circPos.y < size.y * 0.5e0)
 		return true;
-	else if ((Vector2<double>{ -rectangle.GetWidth(), -rectangle.GetHeight() } * 0.5).SquaredMagnitude() < std::pow(radius, 2.0))
-		return true;
-	else if ((Vector2<double>{ -rectangle.GetWidth(), rectangle.GetHeight() } * 0.5).SquaredMagnitude() < std::pow(radius, 2.0))
-		return true;
-	else if ((Vector2<double>{ rectangle.GetWidth(), -rectangle.GetHeight() } * 0.5).SquaredMagnitude() < std::pow(radius, 2.0))
-		return true;
-	else if ((Vector2<double>{ rectangle.GetWidth(), rectangle.GetHeight() } * 0.5).SquaredMagnitude() < std::pow(radius, 2.0))
-		return true;
-	else
-		return false;
-}
-
-double CircleCollider::GetRadius() const
-{
-	return radius;
+	auto vertices = rectangle.GetVertices();
+	for (auto&& vertex : vertices)
+		if (vertex.SquaredMagnitude() < std::pow(radius, 2.e0))
+			return true;
+	return false;
 }
 
 /******************************** RectangleCollider *********************************/
@@ -217,30 +157,22 @@ bool RectangleCollider::DoesCollideWith(const Vector2<double>& relativePosition,
 /// <remarks>relativePositionは自分を基準点とする。</remarks>
 bool RectangleCollider::CheckFor(const Vector2<double>& relativePosition, const LineSegmentCollider& lineSegment) const
 {
-	std::array<Vector2<double>, 5> rect = {  // 自分の四隅の座標。隣同士の差を取ると四辺のベクトルになるようにする。
-		(Vector2<double>{ -width, -height } * 0.5).Rotate(angle),
-		(Vector2<double>{ -width, height } * 0.5).Rotate(angle),
-		(Vector2<double>{ width, height } * 0.5).Rotate(angle),
-		(Vector2<double>{ width, -height } * 0.5).Rotate(angle),
-		(Vector2<double>{ -width, -height } * 0.5).Rotate(angle)
-	};
-	auto direction = MathUtils::PolarCoordinateToVector<double>(lineSegment.GetLength(), lineSegment.GetAngle());
-	std::array<Vector2<double>, 2> endPoints = { relativePosition + direction * 0.5e0, relativePosition - direction * 0.5e0 };
+	auto endPoints = lineSegment.GetEndPoints();
 
 	// 線分の端点が矩形に含まれる場合。
 	for (std::size_t i = 0; i < endPoints.size(); i++) {
 		auto result = true;
-		for (std::size_t j = 1; j < rect.size(); j++)
-			result = result && ((rect[j] - rect[j - 1]).Cross(endPoints[i] - rect[j - 1]) <= 0.e0);
+		for (std::size_t j = 1; j < vertices.size(); j++)
+			result = result && ((vertices[j] - vertices[j - 1]).Cross(endPoints[i] - vertices[j - 1]) <= 0.e0);
 		if (result)
 			return true;
 	}
 
 	// 線分と矩形の辺とが交差する場合。
-	for (std::size_t i = 1; i < rect.size(); i++) {
-		auto diff = rect[i] - rect[i - 1];
+	for (std::size_t i = 1; i < vertices.size(); i++) {
+		auto diff = vertices[i] - vertices[i - 1];
 		auto segment = LineSegmentCollider({ 0.e0, 0.e0 }, std::atan2(diff.y, diff.x), diff.Magnitude());
-		if (CheckFor(relativePosition + (rect[i] + rect[i - 1]) * 0.5e0, segment))
+		if (CheckFor(relativePosition + (vertices[i] + vertices[i - 1]) * 0.5e0, segment))
 			return true;
 	}
 	return false;
@@ -248,21 +180,22 @@ bool RectangleCollider::CheckFor(const Vector2<double>& relativePosition, const 
 
 bool RectangleCollider::CheckFor(const Vector2<double>& relativePosition, const CircleCollider& circle) const
 {
-	// 矩形の中心を座標系の原点に取る。
+	// 矩形が各座標軸と平行になるように回転させる。
 	auto circPos = relativePosition.Rotate(-angle);
-	if (circPos.x > -width * 0.5 && circPos.x < width * 0.5
-			&& circPos.y > -height * 0.5 - circle.GetRadius() && circPos.y < height * 0.5 + circle.GetRadius())
+	auto size = GetSize();
+	if (circPos.x > -size.x * 0.5 && circPos.x < size.x * 0.5
+			&& circPos.y > -size.y * 0.5 - circle.GetRadius() && circPos.y < size.y * 0.5 + circle.GetRadius())
 		return true;
-	else if (circPos.x > -width * 0.5 - circle.GetRadius() && circPos.x < width * 0.5 + circle.GetRadius()
-			&& circPos.y > -height * 0.5 && circPos.y < height * 0.5)
+	else if (circPos.x > -size.x * 0.5 - circle.GetRadius() && circPos.x < size.x * 0.5 + circle.GetRadius()
+			&& circPos.y > -size.y * 0.5 && circPos.y < size.y * 0.5)
 		return true;
-	else if ((Vector2<double>{ -width, -height } * 0.5).SquaredMagnitude() < std::pow(circle.GetRadius(), 2.0))
+	else if (vertices[0].SquaredMagnitude() < std::pow(circle.GetRadius(), 2.0))
 		return true;
-	else if ((Vector2<double>{ -width, height } * 0.5).SquaredMagnitude() < std::pow(circle.GetRadius(), 2.0))
+	else if (vertices[1].SquaredMagnitude() < std::pow(circle.GetRadius(), 2.0))
 		return true;
-	else if ((Vector2<double>{ width, -height } * 0.5).SquaredMagnitude() < std::pow(circle.GetRadius(), 2.0))
+	else if (vertices[2].SquaredMagnitude() < std::pow(circle.GetRadius(), 2.0))
 		return true;
-	else if ((Vector2<double>{ width, height } * 0.5).SquaredMagnitude() < std::pow(circle.GetRadius(), 2.0))
+	else if (vertices[3].SquaredMagnitude() < std::pow(circle.GetRadius(), 2.0))
 		return true;
 	else
 		return false;
@@ -271,35 +204,12 @@ bool RectangleCollider::CheckFor(const Vector2<double>& relativePosition, const 
 // HACK: 何れの矩形の内部にも相手の頂点が含まれない場合は判定できているのか？
 bool RectangleCollider::CheckFor(const Vector2<double>& relativePosition, const RectangleCollider& rectangle) const
 {
-	std::array<Vector2<double>, 5> rect1 = {  // 自分の四隅の座標。隣同士の差を取ると四辺のベクトルになるようにする。
-		(Vector2<double>{ -width, -height } * 0.5).Rotate(angle),
-		(Vector2<double>{ -width, height } * 0.5).Rotate(angle),
-		(Vector2<double>{ width, height } * 0.5).Rotate(angle),
-		(Vector2<double>{ width, -height } * 0.5).Rotate(angle),
-		(Vector2<double>{ -width, -height } * 0.5).Rotate(angle)
-	};
-	std::array<Vector2<double>, 4> rect2 = {  // 相手の四隅の座標。
-		relativePosition + (Vector2<double>{ -rectangle.GetWidth(), -rectangle.GetHeight() } * 0.5).Rotate(rectangle.GetAngle()),
-		relativePosition + (Vector2<double>{ -rectangle.GetWidth(), rectangle.GetHeight() } * 0.5).Rotate(rectangle.GetAngle()),
-		relativePosition + (Vector2<double>{ rectangle.GetWidth(), -rectangle.GetHeight() } * 0.5).Rotate(rectangle.GetAngle()),
-		relativePosition + (Vector2<double>{ rectangle.GetWidth(), rectangle.GetHeight() } * 0.5).Rotate(rectangle.GetAngle())
-	};
-	for (std::size_t i = 0; i < rect2.size(); i++) {
+	for (std::size_t i = 0; i < rectangle.vertices.size() - 1; i++) {
 		auto result = true;
-		for (std::size_t j = 1; j < rect1.size(); j++)
-			result = result && ((rect1[j] - rect1[j - 1]).Cross(rect2[i] - rect1[j - 1]) <= 0.e0);
+		for (std::size_t j = 1; j < this->vertices.size(); j++)
+			result = result && ((this->vertices[j] - this->vertices[j - 1]).Cross(rectangle.vertices[i] - this->vertices[j - 1]) <= 0.e0);
 		if (result)
 			return true;
 	}
 	return false;
-}
-
-double RectangleCollider::GetHeight() const
-{
-	return height;
-}
-
-double RectangleCollider::GetWidth() const
-{
-	return width;
 }
--- shooter2d/collider.h
+++ shooter2d/collider.h
@@ -2,6 +2,7 @@
 #define COLLIDER_H
 
 #include "vector2.h"
+#include <array>
 #include <memory>
 
 namespace Shooter {
@@ -9,11 +10,18 @@ namespace Shooter {
 	class CircleCollider;
 	class RectangleCollider;
 
+	/// <summary>衝突判定をする図形全ての親クラス。図形の形状などは子クラスで定義する。</summary>
+	/// <remarks>
+	///     子クラスで有すメンバ変数には特に制限を設けない。すなわち、必ずしもここで定義されたangleを用いなくても良いとする。
+	///     また、CheckForのrelativePositionにはColliderの中心同士の相対位置を渡す（DoesCollideWithの定義を参照）ので、
+	///     それぞれのメンバ変数はoffsetの値を考慮せずに定義していることに注意。
+	/// </remarks>
 	class Collider
 	{
 	public:
 		/// <param name="offset">保有先のMover::position（スプライトの中心位置）からの相対位置</param>
 		/// <param name="angle">保有先のMover::angleからの相対角度</param>
+		/// <remarks>2次元剛体の自由度から一意に定まる。</remarks>
 		Collider(const Vector2<double>& offset, const double angle) : offset(offset), angle(angle) {}
 		Collider() : offset({0.0, 0.0}), angle(0.0) {}
 		virtual ~Collider() {}
@@ -21,65 +29,142 @@ namespace Shooter {
 		virtual bool CheckFor(const Vector2<double>& relativePosition, const LineSegmentCollider& lineSegment) const = 0;  // 線分との衝突判定。
 		virtual bool CheckFor(const Vector2<double>& relativePosition, const CircleCollider& circle) const = 0;  // 円との衝突判定。
 		virtual bool CheckFor(const Vector2<double>& relativePosition, const RectangleCollider& rectangle) const = 0;  // 矩形との衝突判定。
-		const Vector2<double>& GetOffset() const&;
-		void SetOffset(const Vector2<double>& offset);
-		double GetAngle() const;
-		void SetAngle(const double angle);
+
+		const Vector2<double>& GetOffset() const&
+		{
+			return offset;
+		}
+
+		void SetOffset(const Vector2<double>& offset)
+		{
+			this->offset = offset;
+		}
+
+		double GetAngle() const
+		{
+			return angle;
+		}
+
+		virtual void SetAngle(const double angle)
+		{
+			this->angle = MathUtils::Modulo(angle, 2 * M_PI);  // 負の値が渡されても、0 <= angle < 2 pi になるように変換する。
+		}
 	protected:
 		Vector2<double> offset;  // 保有先のMoverクラスのpositionを局所座標系の原点としたとき、その局所座標系における位置。
-		double angle;  // HACK: 傾き具合を定義するより、各クラスそれぞれに応じてベクトルを使う方がスマートかもしれない。
+		double angle;            // 局所座標系のx軸を基準とした角度。
 	};
 
-	// 局所座標系における原点は線分の中点とする。
+	/// <summary>線分の衝突判定クラス。</summary>
+	/// <remarks>局所座標系における原点は線分の中点とする。</remarks>
 	class LineSegmentCollider : public Collider {
 	public:
-		//LineSegmentCollider(const Vector2<double>& initialPoint, const Vector2<double>& terminalPoint)
-		//	: Collider(initialPoint, 0.0), orientation(terminalPoint - initialPoint) {}
+		/// <param name="offset">保有先のMover::position（スプライトの中心位置）からの相対位置</param>
+		/// <param name="angle">保有先のMover::angleからの相対角度</param>
+		/// <param name="length">線分の全長</param>
 		LineSegmentCollider(const Vector2<double>& offset, const double angle, const double length)
-			: Collider(offset, angle), length(length)
+			: Collider(offset, angle)
+			, endPoints({
+				MathUtils::PolarCoordinateToVector<double>(0.5e0 * length, angle + M_PI),
+				MathUtils::PolarCoordinateToVector<double>(0.5e0 * length, angle)
+			})
 		{}
+
+		LineSegmentCollider(const Vector2<double>& initialPoint, const Vector2<double>& terminalPoint)
+			: endPoints({ initialPoint, terminalPoint })
+		{
+			auto orientation = terminalPoint - initialPoint;
+			Collider::SetOffset(orientation * 0.5e0);
+			Collider::SetAngle(std::atan2(orientation.y, orientation.x));
+		}
+
 		bool DoesCollideWith(const Vector2<double>& relativePosition, Collider& collider) const override;
 		bool CheckFor(const Vector2<double>& relativePosition, const LineSegmentCollider& lineSegment) const override;
 		bool CheckFor(const Vector2<double>& relativePosition, const CircleCollider& circle) const override;
 		bool CheckFor(const Vector2<double>& relativePosition, const RectangleCollider& rectangle) const override;
-		//const Vector2<double>& GetOrientation() const&;
-		//Vector2<double> GetTerminalPoint() const;
-		double GetLength() const;
+
+		double GetLength() const
+		{
+			return (endPoints[1] - endPoints[0]).Magnitude();
+		}
+
+		const std::array<Vector2<double>, 2>& GetEndPoints() const& {
+			return endPoints;
+		}
+
+		void SetAngle(const double angle) override
+		{
+			for (auto&& endPoint : endPoints)
+				endPoint.Rotate(-this->angle).Rotate(angle);
+			Collider::SetAngle(angle);
+		}
 	private:
-		//Vector2<double> orientation;  // 方向ベクトル。
-		double length;  // 線分の全長。
+		std::array<Vector2<double>, 2> endPoints;  // 始点と終点。
 	};
 
-	// 局所座標系における原点は円の中心とする。
+	/// <summary>円の衝突判定クラス。</summary>
+	/// <remarks>局所座標系における原点は円の中心とする。</remarks>
 	class CircleCollider : public Collider {
 	public:
+		/// <param name="offset">保有先のMover::position（スプライトの中心位置）からの相対位置</param>
+		/// <param name="angle">保有先のMover::angleからの相対角度</param>
+		/// <param name="radius">円の半径</param>
 		CircleCollider(const Vector2<double>& offset, const double radius) : Collider(offset, 0.0), radius(radius) {}
 		CircleCollider(const double radius) : Collider(), radius(radius) {}
 		bool DoesCollideWith(const Vector2<double>& relativePosition, Collider& collider) const override;
 		bool CheckFor(const Vector2<double>& relativePosition, const LineSegmentCollider& lineSegment) const override;
 		bool CheckFor(const Vector2<double>& relativePosition, const CircleCollider& circle) const override;
 		bool CheckFor(const Vector2<double>& relativePosition, const RectangleCollider& rectangle) const override;
-		double GetRadius() const;
+
+		double GetRadius() const
+		{
+			return radius;
+		}
 	private:
 		double radius;
 	};
 
-	// 局所座標系における原点は矩形の中心とする。
+	/// <summary>矩形の衝突判定クラス。</summary>
+	/// <remarks>局所座標系における原点は矩形の中心とする。</remarks>
 	class RectangleCollider : public Collider {
 	public:
-		RectangleCollider(const Vector2<double>& offset, const double angle, const double height, const double width)
-			: Collider(offset, angle), height(height), width(width)
+		/// <param name="offset">保有先のMover::position（スプライトの中心位置）からの相対位置</param>
+		/// <param name="angle">保有先のMover::angleからの相対角度</param>
+		/// <param name="size">幅と高さ</param>
+		/// <remarks>sizeには特に正の実数であることを要請しない。</remarks>
+		RectangleCollider(const Vector2<double>& offset, const double angle, const Vector2<double>& size)
+			: Collider(offset, angle)
+			, vertices({
+				((-size) * 0.5e0).Rotate(angle),
+				(Vector2<double>{ -size.x, size.y } * 0.5e0).Rotate(angle),
+				(Vector2<double>{ size.x, -size.y } * 0.5e0).Rotate(angle),
+				(size * 0.5e0).Rotate(angle),
+				((-size) * 0.5e0).Rotate(angle)
+			})
 		{}
-		RectangleCollider(const double height, const double width) : Collider(), height(height), width(width) {}
+		RectangleCollider(const Vector2<double>& size) : RectangleCollider({ 0.e0, 0.e0 }, 0.e0, size) {}
 		bool DoesCollideWith(const Vector2<double>& relativePosition, Collider& collider) const override;
 		bool CheckFor(const Vector2<double>& relativePosition, const LineSegmentCollider& lineSegment) const override;
 		bool CheckFor(const Vector2<double>& relativePosition, const CircleCollider& circle) const override;
 		bool CheckFor(const Vector2<double>& relativePosition, const RectangleCollider& rectangle) const override;
-		double GetHeight() const;
-		double GetWidth() const;
+
+		Vector2<double> GetSize() const
+		{
+			return { (vertices[2] - vertices[1]).Magnitude(), (vertices[0] - vertices[1]).Magnitude() };
+		}
+
+		const std::array<Vector2<double>, 5>& GetVertices() const&
+		{
+			return vertices;
+		}
+
+		void SetAngle(const double angle) override
+		{
+			for (auto&& vertex : vertices)
+				vertex = vertex.Rotate(-this->angle).Rotate(angle);
+			Collider::SetAngle(angle);
+		}
 	private:
-		double height;
-		double width;
+		std::array<Vector2<double>, 5> vertices;  // 四頂点の座標。左下、左上、右上、右下の順。隣同士の差を取ると四辺のベクトルになるようにするため、1成分多い。
 	};
 }
 
